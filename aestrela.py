751ef1796db84e5b0afdeea696d8360f9b35d51e
Renan Siman <31517127+RenanSiman@users.noreply.github.com>
2019-08-30 02:56:01 -0300

    Add files via upload

--- aestrela.py
+++ aestrela.py
@@ -3,7 +3,7 @@ from queue import PriorityQueue
 
 def criaGrafo(file):
     """Builds a weighted, undirected grafo"""
-    grafo = nx.MultiGraph()
+    grafo = nx.Graph()
     for line in file:
         # insere em 'v1' e 'v2' as cidades e em 'w' as distancias
         v1, v2, w = line.split(',')
@@ -11,15 +11,13 @@ def criaGrafo(file):
         v1, v2 = v1.strip(), v2.strip()
         w = int(w.strip())
 
-        # nao é necessaria esta verificacao
-        # if v1 not in grafo:
-            # grafo.add_node(v1)
-        # if v2 not in grafo:
-            # grafo.add_node(v2)
-
         # cria as arestas entre as cidades e define o peso entre elas
         # v1 e v2 ja serao adicionados como 'nodes'
         grafo.add_edge(v1,v2,weight = w)
+
+        # imprimir peso da aresta
+        # print(grafo[v1][v2]['weight'])
+    # print("Numero de nós no grafo: ", len(grafo))
     return grafo
 
 def criaHeuristica():
@@ -46,10 +44,10 @@ def reconstruct_path(came_from, start, goal):
     path.reverse() # optional
     return path
 
-def aestrela(graph, start, goal):
+def aestrela(graphR, start, goal, graphH):
     # cria uma fila de prioridades
     frontier = PriorityQueue()
-    # inicia a fila com a cidade de origem e custo 0
+    # inicia a fila com a nó de origem e o custo = 0
     frontier.put(start, 0)
     came_from = {}
     cost_so_far = {}
@@ -57,41 +55,47 @@ def aestrela(graph, start, goal):
     cost_so_far[start] = 0
 
     while not frontier.empty():
-        #  recebe cidade atual
+        #  recebe o nó atual
         current = frontier.get()
+        # imprime os nós adjacentes do nó atuais e o peso entre as arestas
+        print(graphR.adj[current])
 
-        # destino alcancado
+        # se o objetivo for alcançado
         if current == goal:
             break
 
+        # verifica o peso das adjacências do nó atual
+        for next_node in graphR.adj[current]:
+            # atribui um novo custo para as adjacencias atuais f(n) = g(n) + h(n)
+            new_cost = graphR[current][next_node]['weight'] + graphH[next_node]
 
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
+            if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
+                cost_so_far[next_node] = new_cost
+                priority = new_cost + graphH[next_node]
+                frontier.put(next_node, priority)
+                came_from[next_node] = current
 
-    return came_from, cost_so_far
+    return  cost_so_far #,came_from
 
 
 def main():
-    start = input('Cidade de inicio: ')
-    dest = input('Cidade de destino: ')
+    # start = input('Cidade de inicio: ')
+    # dest = input('Cidade de destino: ')
+    start = "lugoj"
+    dest = "bucharest"
     # abre arquivo de distancias reais somente como leitura
     with open('dist_real.csv', 'r') as file:
         lines = file.readlines()
+
     grafo = criaGrafo(lines)
-    # print(criaHeuristica())
 
     # imprimir 'nós'
-    print(grafo.nodes())
+    # print("Nós do grafo: \n",grafo.nodes(),"\n")
     # imprimir 'arestas'
-    print(grafo.edges())
+    # print("Arestas do grafo: \n",grafo.edges(),"\n")
 
     # chama busca A*
-    # aestrela(grafo, start, dest)
+    print("\n", aestrela(grafo, start, dest, criaHeuristica()))
 
 if __name__ == '__main__':
     main()
