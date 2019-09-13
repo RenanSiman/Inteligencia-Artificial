8e7fa7ed8df23886a748caf3760e2a21fccec85e
Igor Oliveira <m.igoroliveira@gmail.com>
2019-09-13 17:12:17 -0300

    Update jogoVelha.c

--- jogoVelha.c
+++ jogoVelha.c
@@ -2,11 +2,11 @@
 
 char peca(int i) {
     switch(i) {
-        case 1:
+        case -1:
             return 'X';
         case 0:
             return ' ';
-        case 2:
+        case 1:
             return 'O';
     }
 }
@@ -30,37 +30,96 @@ int win(const int tabuleiro[9]) {
     }
     return 0;
 }
+int minmax(int tabuleiro[9], int jogador) {
+    int vencedor = win(tabuleiro);
+    if(vencedor != 0) 
+        return vencedor*jogador; // verifica se não há vencedor
+    int move = -1;
+    int pontos = -2;
+    int i;
+    for(i = 0; i < 9; ++i) {//percorre o tabuleiro
+        if(tabuleiro[i] == 0) {//verifica se está vazio
+            tabuleiro[i] = jogador;
+            int auxpontos = -1 * minmax(tabuleiro, jogador*-1);
+            if(auxpontos > pontos) {
+                pontos = auxpontos;
+                move = i;
+            }
+            tabuleiro[i] = 0;//reinicia
+        }
+    }
+    if(move == -1) 
+        return 0;
+    return pontos;
+}
+
+
+void computador(int tabuleiro[9], int jogador) {
+    int posicao = 0, oponente;
+    char c;
+    if(jogador == -1){
+        oponente = 1;
+        c = 'X';
+    }
+    else{
+        oponente = -1;
+        c = 'O';
+    }
+    int move = -1;
+    int pontos = -2;
+    int i;
+    for(i = 0; i < 9; i++) {
+        if(tabuleiro[i] == 0) {
+            tabuleiro[i] = 1;
+            int tpontos = -1 * minmax(tabuleiro, oponente);
+            tabuleiro[i] = 0;
+            if(tpontos > pontos) {
+                pontos = tpontos;
+                move = i;
+            }
+        }
+    }
+    //returns a score based on minimax tree at a given node.
+    tabuleiro[move] = 1;
+}
 
 void joga(int tabuleiro[9], int jogador) {
     int posicao = 0, oponente;
-    if(jogador == 1)
-        oponente = 2;
-    else
+    char c;
+    if(jogador == -1){
         oponente = 1;
+        c = 'X';
+    }
+    else{
+        oponente = -1;
+        c = 'O';
+    }
     do {
-        printf("\nJogador %d insira posicao de 1 a 9: ", jogador);
+        printf("\nJogador %c insira posicao de 1 a 9: ", c);
         scanf("%d", &posicao);
         posicao --;
         printf("\n");
-    } while (posicao >= 9 || posicao < 0 && tabuleiro[posicao] == 0 || tabuleiro[posicao] == oponente );
+    } while (posicao >= 9 || posicao < 0 && tabuleiro[posicao] == 0 || tabuleiro[posicao] == oponente || tabuleiro[posicao] == jogador );
     tabuleiro[posicao] = jogador;
 }
 
+
 int main() {
     int tabuleiro[9] = {0,0,0,0,0,0,0,0,0};
-    printf("Jogador 1: O, Jogador 2: X\n");
+    printf("Jogador 1: X, Computador: O\n");
     unsigned vez;
     int jogador;
     for(vez = 0; vez < 9 && win(tabuleiro) == 0; ++vez) {
         if(vez % 2 == 0){
-            jogador = 1;
+            jogador = -1;
             desenha(tabuleiro);
             joga(tabuleiro,jogador);
         }
         else {
-            jogador = 2;
+            jogador = 1;
             desenha(tabuleiro);
-            joga(tabuleiro,jogador);
+            printf("Vez do computador\n");
+            computador(tabuleiro,jogador);
         }
     }
     switch(win(tabuleiro)) {
@@ -68,13 +127,13 @@ int main() {
             desenha(tabuleiro);
             printf("Velha\n");
             break;
-        case 1:
+        case -1:
             desenha(tabuleiro);
             printf("Jogador 1 venceu\n");
             break;
-        case 2:
+        case 1:
             desenha(tabuleiro);
-            printf("Jogador 2 venceu\n");
+            printf("Computador venceu\n");
             break;
     }
 }
